21692f792bdfb2054e99e0503bb0f264e1184d1b
ian <zyktnn@gmail.com>
2017-02-18 21:50:18 +0300

    add conversion to grayscale

---
+++ monochromer.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+
+def convert(path, output_path='output.png'):
+    """
+    Converts an image at :path: to grayscale and saves it at :output_path:
+    """
+    img = Image.open(path)
+    out = img.convert('L')
+    out.save(output_path)
+
+
+if __name__ == '__main__':
+    convert('image/image.png', 'image/output.png')
